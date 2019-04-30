f84f6694c9f1c1e76b5c2ecd82add1baa5a3d93f
Derek Miranda <derek.miranda@ff0000.com>
2019-04-29 17:48:50 -0700

    pkg: git init
    - 800x250 layouts complete
    - redist and repub

---
+++ src/1-build/800x250/_c20-tracker/ad0caf31-2ab1-4ed6-81e8-b25e84412d71.js
@@ -0,0 +1,32 @@
+// GENERIC SOURCE TRACKER: bt-Endframe-Resolve
+if (typeof module === 'undefined') {
+	module = {}
+}
+// prettier-ignore
+module.exports = {
+  "id": "ad0caf31-2ab1-4ed6-81e8-b25e84412d71",
+  "name": "bt-ER-800x250",
+  "label": "Right Corner",
+  "description": "C20/Monet-compatible templates that use  https://github.com/ff0000-ad-tech/ad-docs/blob/master/README.md",
+  "type": "build_templates",
+  "builder": {
+    "label": "800x250",
+    "tier": "endframe-resolve",
+    "size": "800x250",
+    "creative": "./3-traffic/default/800x250",
+    "layouts": {
+      "SIDE_BY_SIDE_LEFT": "Side-By-Side Left",
+      "SIDE_BY_SIDE_CENTER": "Side-By-Side Center",
+      "SIDE_BY_SIDE_RIGHT": "Side-By-Side Right",
+      "STACKED_LEFT": "Stacked Left",
+      "STACKED_CENTER": "Stacked Center"
+    }
+  },
+  "context": "RED Interactive",
+  "state": "published",
+  "updated": 1556585234660,
+  "username": "GitHub",
+  "full_name": "NetflixDev/bt-ER-800x250",
+  "html_url": "https://github.com/NetflixDev/bt-ER-800x250",
+  "version": "2.2.1"
+};

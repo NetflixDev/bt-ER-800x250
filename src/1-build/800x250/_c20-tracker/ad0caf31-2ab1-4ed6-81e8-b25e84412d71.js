2605d78f7fb93b47d46b13044e0f0c669ff88e92
Derek Miranda <derek.miranda@ff0000.com>
2019-05-15 18:24:50 -0700

    feature: adds supercut option

--- src/1-build/800x250/_c20-tracker/ad0caf31-2ab1-4ed6-81e8-b25e84412d71.js
+++ src/1-build/800x250/_c20-tracker/ad0caf31-2ab1-4ed6-81e8-b25e84412d71.js
@@ -6,27 +6,27 @@ if (typeof module === 'undefined') {
 module.exports = {
   "id": "ad0caf31-2ab1-4ed6-81e8-b25e84412d71",
   "name": "bt-ER-800x250",
+  "label": "Right Corner",
   "description": "C20/Monet-compatible templates that use  https://github.com/ff0000-ad-tech/ad-docs/blob/master/README.md",
   "type": "build_templates",
   "builder": {
-    "label": "800x250 NF Global Template",
+    "label": "800x250",
     "tier": "endframe-resolve",
     "size": "800x250",
-    "creative": "./dist/3-traffic/default/800x250",
-    "toolkit": "./toolkit/800x250_NF_Global_Template_ContainerRecycle_Layered.psd",
+    "creative": "./3-traffic/default/800x250",
     "layouts": {
       "SIDE_BY_SIDE_LEFT": "Side-By-Side Left",
       "SIDE_BY_SIDE_CENTER": "Side-By-Side Center",
       "SIDE_BY_SIDE_RIGHT": "Side-By-Side Right",
-      "STACKED": "Stacked"
+      "STACKED_LEFT": "Stacked Left",
+      "STACKED_CENTER": "Stacked Center"
     }
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1557791149708,
+  "updated": 1556664710368,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-800x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-800x250",
-  "version": "2.2.1",
-  "minimum": "2.2.1"
+  "version": "2.2.1"
 };

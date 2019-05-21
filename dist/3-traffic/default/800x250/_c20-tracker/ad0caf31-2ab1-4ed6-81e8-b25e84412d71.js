d33b175f6e17d0daef9152d473d010b719c04d38
Greg Connell <greg.connell@ff0000.com>
2019-05-21 14:53:51 -0700

    adds manifest

--- dist/3-traffic/default/800x250/_c20-tracker/ad0caf31-2ab1-4ed6-81e8-b25e84412d71.js
+++ dist/3-traffic/default/800x250/_c20-tracker/ad0caf31-2ab1-4ed6-81e8-b25e84412d71.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1557536071685,
+  "updated": 1558475610167,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-800x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-800x250",

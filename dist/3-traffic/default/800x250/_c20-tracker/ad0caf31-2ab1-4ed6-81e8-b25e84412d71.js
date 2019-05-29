a1035116ca0f71bc1c997b06cd3bff0a7f0bebb5
Derek Miranda <derek.miranda@ff0000.com>
2019-05-29 11:31:13 -0700

    package: removes label key + redist

--- dist/3-traffic/default/800x250/_c20-tracker/ad0caf31-2ab1-4ed6-81e8-b25e84412d71.js
+++ dist/3-traffic/default/800x250/_c20-tracker/ad0caf31-2ab1-4ed6-81e8-b25e84412d71.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1558654020712,
+  "updated": 1559154658551,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-800x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-800x250",

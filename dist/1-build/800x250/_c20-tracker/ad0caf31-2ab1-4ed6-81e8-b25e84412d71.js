0f568e5ec7ea7ac2526e8beafbfd27c619e49fef
Derek Miranda <derek.miranda@ff0000.com>
2019-05-30 16:55:54 -0700

    feature: toggleable N-slide animation
    - redist

--- dist/1-build/800x250/_c20-tracker/ad0caf31-2ab1-4ed6-81e8-b25e84412d71.js
+++ dist/1-build/800x250/_c20-tracker/ad0caf31-2ab1-4ed6-81e8-b25e84412d71.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1559154658551,
+  "updated": 1559262929102,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-800x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-800x250",

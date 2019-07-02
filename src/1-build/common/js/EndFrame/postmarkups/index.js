81fbcca57bf74b01120aa5d88e8ff94f98d619c8
Derek Miranda <derek.miranda@ff0000.com>
2019-07-02 11:55:12 -0700

    cleans: removes TT images
    - repub

--- src/1-build/common/js/EndFrame/postmarkups/index.js
+++ src/1-build/common/js/EndFrame/postmarkups/index.js
@@ -1,37 +1,41 @@
-import { createSideBySidePostMarkups } from './horizontalSideBySide.js'
-import horizontalStacked from './horizontalStacked.js'
-import { Align } from 'ad-view'
-import { ObjectUtils } from 'ad-utils'
+import { createSideBySidePostMarkups } from "./horizontalSideBySide.js";
+import horizontalStacked from "./horizontalStacked.js";
+import { Align } from "ad-view";
+import { ObjectUtils } from "ad-utils";
 
-const { leftPostMarkup, centerPostMarkup, rightPostMarkup } = createSideBySidePostMarkups({
-	defaultArgs: {
-		ctaLogoOffset: 14,
-		tuneInFontSize: 18,
-		tuneInLockupOffset: 8
-	},
-	yAlign: {
-		type: Align.BOTTOM,
-		offset: -26
-	},
-	layoutXAligns: {
-		leftXAlign: {
-			type: Align.LEFT,
-			against: 70
-		},
-		centerXAlign: Align.CENTER,
-		rightXAlign: {
-			type: Align.RIGHT,
-			offset: -73
-		}
-	}
-})
+const {
+  leftPostMarkup,
+  centerPostMarkup,
+  rightPostMarkup
+} = createSideBySidePostMarkups({
+  defaultArgs: {
+    ctaLogoOffset: 14,
+    tuneInFontSize: 18,
+    tuneInLockupOffset: 8
+  },
+  yAlign: {
+    type: Align.BOTTOM,
+    offset: -26
+  },
+  layoutXAligns: {
+    leftXAlign: {
+      type: Align.LEFT,
+      against: 70
+    },
+    centerXAlign: Align.CENTER,
+    rightXAlign: {
+      type: Align.RIGHT,
+      offset: -73
+    }
+  }
+});
 
 function stackedPostMarkup() {
-	horizontalStacked({
-		brandingLockupOffset: 16,
-		brandingLockupRightPadding: 30,
-		tuneInFontSize: 18
-	})
+  horizontalStacked({
+    brandingLockupOffset: 16,
+    brandingLockupRightPadding: 30,
+    tuneInFontSize: 18
+  });
 }
 
-export { leftPostMarkup, centerPostMarkup, rightPostMarkup, stackedPostMarkup }
+export { leftPostMarkup, centerPostMarkup, rightPostMarkup, stackedPostMarkup };

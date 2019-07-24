8367fe2273bc38014d9e94e39284042e93598e4f
Derek Miranda <derek.miranda@ff0000.com>
2019-07-24 16:01:13 -0700

    bt-ER-800x250

--- src/3-traffic/default/800x250/_c20-tracker/ad0caf31-2ab1-4ed6-81e8-b25e84412d71.js
+++ src/3-traffic/default/800x250/_c20-tracker/ad0caf31-2ab1-4ed6-81e8-b25e84412d71.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1563402357866,
+  "updated": 1564009131230,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-800x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-800x250",

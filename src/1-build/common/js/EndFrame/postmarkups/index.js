08f0f05602eb32dcd6a15c428edc41781c5eb407
Derek Miranda <derek.miranda@ff0000.com>
2019-09-30 15:56:16 -0700

    Applies any remaining layout fixes
    - redist

--- src/1-build/common/js/EndFrame/postmarkups/index.js
+++ src/1-build/common/js/EndFrame/postmarkups/index.js
@@ -1,41 +1,37 @@
-import { createSideBySidePostMarkups } from "./horizontalSideBySide.js";
-import horizontalStacked from "./horizontalStacked.js";
-import { Align } from "ad-view";
-import { ObjectUtils } from "ad-utils";
+import { createSideBySidePostMarkups } from './horizontalSideBySide.js'
+import horizontalStacked from './horizontalStacked.js'
+import { Align } from 'ad-view'
+import { ObjectUtils } from 'ad-utils'
 
-const {
-  leftPostMarkup,
-  centerPostMarkup,
-  rightPostMarkup
-} = createSideBySidePostMarkups({
-  defaultArgs: {
-    ctaLogoOffset: 14,
-    headlineFontSize: 18,
-    headlineLockupOffset: 8
-  },
-  yAlign: {
-    type: Align.BOTTOM,
-    offset: -26
-  },
-  layoutXAligns: {
-    leftXAlign: {
-      type: Align.LEFT,
-      against: 70
-    },
-    centerXAlign: Align.CENTER,
-    rightXAlign: {
-      type: Align.RIGHT,
-      offset: -73
-    }
-  }
-});
+const { leftPostMarkup, centerPostMarkup, rightPostMarkup } = createSideBySidePostMarkups({
+	defaultArgs: {
+		ctaLogoOffset: 14,
+		headlineFontSize: 18,
+		headlineLockupOffset: 8
+	},
+	yAlign: {
+		type: Align.BOTTOM,
+		offset: -26
+	},
+	layoutXAligns: {
+		leftXAlign: {
+			type: Align.LEFT,
+			against: 69
+		},
+		centerXAlign: Align.CENTER,
+		rightXAlign: {
+			type: Align.RIGHT,
+			offset: -73
+		}
+	}
+})
 
 function stackedPostMarkup() {
-  horizontalStacked({
-    brandingLockupOffset: 16,
-    brandingLockupRightPadding: 30,
-    headlineFontSize: 18
-  });
+	horizontalStacked({
+		brandingLockupOffset: 16,
+		brandingLockupRightPadding: 30,
+		headlineFontSize: 18
+	})
 }
 
-export { leftPostMarkup, centerPostMarkup, rightPostMarkup, stackedPostMarkup };
+export { leftPostMarkup, centerPostMarkup, rightPostMarkup, stackedPostMarkup }
